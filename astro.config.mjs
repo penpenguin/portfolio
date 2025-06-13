2316732914961d87ecec27ded2064a7d923a0e14
penpenguin <70111306+penpenguin@users.noreply.github.com>
2025-06-13 16:54:07 +0900

    fix: GitHub Pages用にベースパスを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://penpenguin.github.io',
+  base: '/portfolio',
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed',
